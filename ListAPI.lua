6db1929743ee2857d7364d3f008a5628d8a27cf9
QVGK <45526481+QVGK@users.noreply.github.com>
2022-12-30 14:03:46 +0800

    Create ListAPI.lua

---
+++ ListAPI.lua
@@ -0,0 +1,37 @@
+-- // Services \\ --
+local HttpService = game:GetService('HttpService')
+
+-- // Main Code \\ --
+return function(Key, Token)
+	local TrelloAPI = {}
+	
+	function TrelloAPI:GetListID(BoardID: string, ListName: string)
+		local lists
+		
+		if not BoardID or not ListName then
+			warn('GetListID failed. Missing arguments.')
+			return false
+		end
+		
+		local s, e = pcall(function()
+			local tempr = HttpService:GetAsync('https://api.trello.com/1/boards/'..BoardID..'/lists?key='..Key..'&token='..Token)
+			lists = HttpService:JSONDecode(tempr)
+		end)
+		
+		if not s then
+			warn('GetListID failed.')
+			warn(e)
+			return false 
+		end
+		
+		for _, Object in pairs(lists) do
+			if ListName == Object.name then
+				warn('GetListID successful.')
+				return Object.id
+			end
+		end
+		
+	end
+
+	return TrelloAPI
+end
